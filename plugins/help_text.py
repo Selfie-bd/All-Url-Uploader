6e35b8e26ab3814a11a8d27adfdd8e5791fb4350
Selfie SD <webtamil0@gmail.com>
2022-05-14 12:55:23 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -41,7 +41,7 @@ async def help_user(bot, update):
 @Clinton.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
   await bot.send_message(
-    chat_id=message.chat.id,
+    chat_id=update.chat.id,
     text=Translation.START_TEXT,
     reply_to_message_id=update.message_id
   )
